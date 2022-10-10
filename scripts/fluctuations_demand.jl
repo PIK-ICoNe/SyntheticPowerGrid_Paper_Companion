7af8582bb10394547f3e04f04c1d01c46ad77333
buettner <buettner@pik-potsdam.de>
2022-10-10 09:50:39 +0200

    Demand Model

--- scripts/fluctuations_demand.jl
+++ scripts/fluctuations_demand.jl
@@ -36,30 +36,31 @@ Q_set = map(i -> nodes[i].Q, fluc_node_idxs)
 ##
 # Using an intermittent wind power fluctuation Langevin-type model to generate fluctuating time series
 tspan = (0.0, 100.0)
-D = 0.1 # Intermittence strength
 p = 0.2 # Penetration parameter
-x, t = wind_power_model(tspan, D = D)
-x_inter = linear_interpolation(t, x) # Interpolate the time series
+P_fluc, t = load_profile_model(tspan)
+P_mean = mean(P_fluc)
+P_fluc = (P_fluc .- P_mean) ./ P_mean
+P_fluc_inter = linear_interpolation(t, P_fluc) # Interpolate the time series
 
-plot(t, x, idxs = 1, xlabel = "t[s]", ylabel = "x(t)", label = "Time series", lw = 3)
-plot!(t, x_inter(t), idxs = 1,label = "Interpolated time series", line_style = :dash, xlabel = "t[s]", ylabel = "x(t)")
+plot(t, P_fluc, idxs = 1, xlabel = "t[s]", ylabel = "P_fluc(t)", label = "Time series", lw = 3)
+plot!(t, P_fluc_inter(t), idxs = 1,label = "Interpolated time series")
 
 ##
 # Multi Node Fluctuations, completely correlated, exchange all PQAlgebraic with FluctuationNode
-fluctuations_corr = map(f -> FluctuationNode(t -> P_set[f] + p * x_inter(t), t -> Q_set[f]), 1:length(fluc_node_idxs))
-pg_wind_corr = generate_powergrid_fluctuations(pg, fluc_node_idxs, fluctuations_corr)
+fluctuations_corr = map(f -> FluctuationNode(t -> P_set[f] + p * P_fluc_inter(t), t -> Q_set[f]), 1:length(fluc_node_idxs))
+pg_demand_corr = generate_powergrid_fluctuations(pg, fluc_node_idxs, fluctuations_corr)
 
 ##
 # Simulate a trajectory
 ode = ODEProblem(rhs(pg_wind_corr), op.vec, tspan)
 sol = solve(ode, Rodas4())
 
-solution2 = PowerGridSolution(sol, pg_wind_corr)
+solution2 = PowerGridSolution(sol, pg_demand_corr)
 hline([P_set[1]], label = "Set Point", alpha = 0.3, c = :black)
 plot!(solution2, [fluc_node_idxs[1]], label = "Active Power",:p, lw = 3, ylabel = L"P[p.u.]", xlabel = L"t[s]")
 
 plt2 = plot(solution2, ω_indices, :x_1, legend = false, ylabel = L"ω[rad / s]", xlabel = L"t[s]")
-savefig(plt2, "plots/wind_fluc/multi_node_fluc_correlated.pdf")
+savefig(plt2, "plots/demand_fluc/multi_node_demand_fluc_correlated.pdf")
 
 calculate_performance_measures(solution2) # calculate performance measures
 
@@ -67,22 +68,35 @@ calculate_performance_measures(solution2) # calculate performance measures
 # Multi Node Fluctuations , completely uncorrelated, exchange all PQAlgebraic with FluctuationNode
 # Generate a time series for each node
 
-flucs = [wind_power_model(tspan, D = D) for x in 1:length(fluc_node_idxs)]
-x_inter = map(f -> linear_interpolation(flucs[f][2], flucs[f][1]), 1:length(fluc_node_idxs)) # Interpolate the time series
-fluctuations_uncorr = map(f -> FluctuationNode(t -> P_set[f] + p * x_inter[f](t), t -> Q_set[f]), 1:length(fluc_node_idxs))
 
+P_fluc_inter = linear_interpolation(t, P_fluc) # Interpolate the time series
+
+
+flucs = [load_profile_model(tspan) for x in 1:length(fluc_node_idxs)]
+t = map(f -> flucs[f][2], 1:length(fluc_node_idxs))
+P_mean = map(f -> mean(flucs[f][1]), 1:length(fluc_node_idxs))
+P_flucs = map(f -> (flucs[f][1] .- P_mean[f]) ./ P_mean[f], 1:length(fluc_node_idxs))
+
+P_fluc_inter = map(f -> linear_interpolation(t[f], P_flucs[f]), 1:length(fluc_node_idxs)) # Interpolate the time series
+fluctuations_uncorr = map(f -> FluctuationNode(t -> P_set[f] + p * P_fluc_inter[f](t), t -> Q_set[f]), 1:length(fluc_node_idxs))
+
+##
+fluc_idx = 3
+plot(t[fluc_idx], P_flucs[fluc_idx], idxs = 1, xlabel = "t[s]", ylabel = "P_fluc(t)", label = "Time series", lw = 3)
+plot!(t[fluc_idx], P_fluc_inter[fluc_idx](t[fluc_idx]), idxs = 1,label = "Interpolated time series")
+
+##
 pg_wind_uncorr = pg_wind_corr = generate_powergrid_fluctuations(pg, fluc_node_idxs, fluctuations_uncorr)
 
 ##
 # Simulate a trajectory
-
 ode = ODEProblem(rhs(pg_wind_uncorr), op.vec, tspan)
 sol = solve(ode, Rodas4())
 
 solution3 = PowerGridSolution(sol, pg_wind_uncorr)
-plot!(solution3, fluc_node_idxs, label = "Active Power",:p, lw = 3, ylabel = L"P[p.u.]", xlabel = L"t[s]")
+plot!(solution3, fluc_node_idxs, label = "Active Power",:p, lw = 3, ylabel = L"P[p.u.]", xlabel = L"t[s]", legend = false)
 
 plt3 = plot(solution3, ω_indices, :x_1, legend = false, ylabel = L"ω[rad / s]", xlabel = L"t[s]")
-savefig(plt3, "plots/wind_fluc/multi_node_fluc_uncorrelated.pdf")
+savefig(plt3, "plots/demand_fluc/multi_node_fluc_demand_uncorrelated.pdf")
 
 calculate_performance_measures(solution3) # calculate performance measures
