1c764d4c2ccd60b54c840dac8750e17b48a02c20
buettner <buettner@pik-potsdam.de>
2023-06-07 12:49:42 +0200

    Compare pdfs to normal distribution

--- scripts/compare_frequency_statistics.jl
+++ scripts/compare_frequency_statistics.jl
@@ -11,6 +11,7 @@ using DelimitedFiles
 using KernelDensity
 using StatsBase
 using Colors
+using Plots.Measures
 
 default(grid = false, foreground_color_legend = nothing, bar_edges = false,  lw=3, framestyle =:box, msc = :auto, dpi=300, legendfontsize = 18, labelfontsize = 18, tickfontsize = 15)
 
@@ -49,13 +50,28 @@ savefig(plt_ac, "plots/autocorrelation.pdf")
 
 ## Probability Density Function
 pdf_demand = kde(vcat(f_demand...))
+
+d_d_n = fit(Normal{Float64}, vcat(f_demand...))
+pdf_d_n = map(i -> pdf(d_d_n, pdf_demand.x[i]), 1:length(pdf_demand.x))
+
 pdf_wind = kde(vcat(f_wind...))
+d_w_n = fit(Normal{Float64}, vcat(f_wind...))
+pdf_w_n = map(i -> pdf(d_w_n, pdf_wind.x[i]), 1:length(pdf_wind.x))
+
 pdf_solar = kde(vcat(f_solar...))
+d_s_n = fit(Normal{Float64}, vcat(f_solar...))
+pdf_s_n = map(i -> pdf(d_s_n, pdf_solar.x[i]), 1:length(pdf_solar.x))
 
-plot(pdf_solar.x, pdf_solar.density ./ findmax(pdf_solar.density)[1], c = colorant"goldenrod2", label = L"f_s", xaxis = L"\Delta f [Hz]")
-plot!(pdf_wind.x, pdf_wind.density ./ findmax(pdf_wind.density)[1], label = L"f_w", c = colorant"teal", ls = :dashdot)
-plt_pdf = plot!(pdf_demand.x, pdf_demand.density ./ findmax(pdf_demand.density)[1], label = L"f_d", c = colorant"coral", ls = :dot)
+plot(pdf_solar.x, pdf_solar.density ./ findmax(pdf_solar.density)[1], c = colorant"goldenrod3", label = L"f_s", yaxis = L"PDF")
+plt_pdf_s = plot!(pdf_solar.x, pdf_s_n ./ findmax(pdf_s_n)[1], label = "", c = colorant"goldenrod3", ls = :dashdot, alpha = 0.7)
 
+plot(pdf_wind.x, pdf_wind.density ./ findmax(pdf_wind.density)[1], label = L"f_w", c = colorant"teal")
+plt_pdf_w = plot!(pdf_wind.x, pdf_w_n ./ findmax(pdf_w_n)[1], label = "", c = colorant"teal", ls = :dashdot, alpha = 0.7)
+
+plot(pdf_demand.x, pdf_demand.density ./ findmax(pdf_demand.density)[1], label = L"f_d", c = colorant"coral")
+plt_pdf_d = plot!(pdf_demand.x, pdf_d_n ./ findmax(pdf_d_n)[1], label = "", c = colorant"coral", ls = :dashdot, alpha = 0.7)
+
+plt_pdf = Plots.plot(plt_pdf_s, plt_pdf_w, plt_pdf_d; layout=(1, 3), size=(1500, 500), xaxis = L"\Delta f [Hz]", left_margin=10mm, bottom_margin = 10mm)
 savefig(plt_pdf, "plots/pdf_frequency.pdf")
 
 ## Frequency Increment Statistics
